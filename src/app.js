50db1561fce9b0a637b32d37711658996320d518
nikolayevolution <stu2001321014@uni-plovdiv.bg>
2022-03-04 18:44:29 +0200

    Finish setting up the project

--- src/app.js
+++ src/app.js
@@ -1,91 +1,37 @@
 /* eslint-disable no-undef */
-import "bootstrap";
-import "./assets/styles/main.scss";
-// import './components/Navbar.js'
-const $ = require("jquery");
+import { Router, Routes } from "./libs/router.js";
+import "./components/Navbar.js";
+import "./components/Drawer.js";
+import "./components/Modal.js";
+import "./assets/scripts/app";
 
-window.JQuery = $;
-window.$ = $;
+$(document).ready(function(){
+    if(window.location.hash === "") window.location.hash = "home";
+    $("a").on("click", function(){
+        let hash = $(this).attr("href").split("#")[1];
+        window.location.hash = hash;
+    });
+    const singInBtn = $("#singInBtn");
 
-class Navbar extends HTMLElement {
-    connectedCallback() {
-        // eslint-disable-next-line spaced-comment
-        this.innerHTML = /*html*/ `
-            <header class="__navbar-wrapper">
-                <div class="__navbar-container">
-                    <nav class="__navbar-menu">
-                        <a class="burger">
-                            <i class="bi bi-list"></i>
-                        </a>
-                        <ul class="navbar-nav">
-                            <li class="nav-item">
-                                <a class="logo">7GAG</a>
-                            </li>
-                            <li>
-                                 <a class="#">🔀  Shuffle</a>
-                            </li>
-                            <li>
-                                <a class="#">📱 Get App</a>
-                            </li>
-                            <li>
-                                <a class="#">🔞</a>
-                            </li>
-                            <li>
-                                <a class="#">Donate</a>
-                            </li>
-                            <li>
-                                <a class="#">🇺🇦</a>
-                            </li>
-                            <li>
-                                <a class="#">Crypto</a>
-                            </li>
-                            <li>
-                                <a class="#">ಠ_ಠ</a>
-                            </li>
-                        </ul>
-                        <div class="d-flex __right-menu">
-                            <div class="__btn-group">
-                                <ul class="__navbar-menu-right">
-                                    <li class="nav-item dropdown">
-                                        <a class="nav-link" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                             <i class="bi bi-search"></i>
-                                        </a>
-                                        <ul class="dropdown-menu search-dropdown" aria-labelledby="navbarDropdown">
-                                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                                        </ul>
-                                    </li>
-                                    <li class="nav-item">
-                                        <i class="bi bi-chat-right-fill"></i>
-                                    </li>
-                                     <li class="nav-item">
-                                        <button class="btn __btn-secondary wave">Login</button>
-                                    </li>
-                                    <li class="nav-item">
-                                        <button class="btn __btn-primary">Sing up</button>
-                                    </li>
-                                    <li class="nav-item dropdown">
-                                        <a class="nav-link" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                             <i class="bi bi-chevron-down"></i>
-                                        </a>
-                                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                            <li><a class="dropdown-item" href="#">Sign up or Login</a></li>
-                                             <li><a class="dropdown-item" href="#">Manage consent</a></li>
-                                            <li><a class="dropdown-item" href="#">Dark Mode</a></li>
-                                            <li><a class="dropdown-item" href="#">Download 9GAG app</a></li>
-                                            <li><a class="dropdown-item" href="#">Help center</a></li>
-                                            <li><a class="dropdown-item" href="#">Report problems</a></li>
-                                        </ul>
-        
-                                    </li>
-                                </ul>
-                             </div>
-                        </div>
-                    </nav>
-                </div>
-            </header>
-        `;
-    }
-}
+    $(singInBtn).on("click", function(){
+        $("#exampleModalCenter").modal("show");
+    });
+});
 
-customElements.define("app-navbar", Navbar);
-
+(() => {
+  const routeConfig = [
+    new Routes({
+      path: "fresh",
+      url: "./fresh.html",
+    }),
+    new Routes({
+        path: "trending",
+        url: "./trending.html"
+    }),
+    new Routes({
+        path: "home", 
+        url: "./home.html"
+    })
+  ];
+  new Router(routeConfig, "app");
+})();
---
+++ src/components/Drawer.js
@@ -0,0 +1,66 @@
+// eslint-disable-next-line no-unused-vars
+class Navbar extends HTMLElement {
+  connectedCallback() {
+    // eslint-disable-next-line spaced-comment
+    this.innerHTML = /*html*/ `
+        <div class="drawer-wrapper"> 
+        <div class="drawer-content">
+            <div class="mini-card"> 
+                <div class="card-content"> 
+                    <h2>New to 7GAG?</h2>
+                    <span class="info-text">Sign up now to see more content!</span>
+                    <br />
+                    <button class="btn __btn-primary">Sing up</button>
+                </div>
+            </div>
+            <div class="trending-container"> 
+                <ul class="trending-menu"> 
+                    <li class="nav-item"> 
+                        <h6>&nbsp;&nbsp;&nbsp;7GAG</h6>
+                    </li>
+                    <li class="nav-item nav-sidebar">
+                        <div> 
+                            <i class="bi bi-house-fill"></i>
+                            <a href="#">Hot</a>
+                        </div>   
+                        <a class="nav-link" href="#" id="navbarDropdown" data-bs-toggle="dropdown" aria-expanded="false">       
+                            <i class="bi bi-three-dots"></i>
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">Top Posts</a></li>
+                            <li><a class="dropdown-item" href="#">Most Recent</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item"> 
+                        <i class="bi bi-graph-up-arrow"></i>
+                        <a href="#trending">Trending</a>
+                    </li>
+                    <li class="nav-item"> 
+                        <i class="bi bi-clock"></i>
+                        <a href="#fresh">Fresh</a>
+                    </li>
+                    <li class="nav-item"> 
+                        <i class="bi bi-bar-chart-line-fill"></i>
+                        <a href="#">Top</a>
+                    </li>
+                    <li class="nav-item nav-sidebar"> 
+                        <div> 
+                            <i class="bi bi-geo-alt-fill"></i>
+                            <a href="#">Bulgaria 🇧🇬</a>
+                        </div>
+                        <a class="nav-link" href="#" id="navbarDropdown" data-bs-toggle="dropdown" aria-expanded="false">             
+                            <i class="bi bi-three-dots"></i>
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">Sign up or Login</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+        `;
+  }
+}
+
+customElements.define("app-drawer", Navbar);
