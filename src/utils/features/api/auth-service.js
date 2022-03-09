1d821357b546e52014aa0ff1e5e8531f323daecf
Nikolay Pleshkov <47944944+nikolaypleshkov@users.noreply.github.com>
2022-03-09 17:50:37 +0200

    Add: add basic CRUD operation functions and firebase auth emulator)

---
+++ src/utils/features/api/auth-service.js
@@ -0,0 +1,10 @@
+import { getFirestore } from 'firebase/firestore/lite';
+import { app } from '../firebase';
+const db = getFirestore(app);
+async function signIn(email, password){
+
+}
+
+async function signUp(fullName, email, password){
+
+}
