65ed9dc53b2662584c1482f73554ed5f3d7bbdce
Nikolay Pleshkov <47944944+nikolaypleshkov@users.noreply.github.com>
2022-03-09 17:51:33 +0200

    Update: Remove unused functions in firebase.js

--- src/utils/features/firebase.js
+++ src/utils/features/firebase.js
@@ -1,9 +1,5 @@
 /* eslint-disable no-unused-vars */
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore, collection, getDocs} from "firebase/firestore/lite";
-//Follow this pattern to import others Firebase services;
-//import { } from 'firebase/<service>';
 
 const firebaseConfig = {
     apiKey: "AIzaSyAC__9P6aYVonp5t3aHqvaHju3ld5vSETA",
@@ -14,11 +10,4 @@ const firebaseConfig = {
     appId: "1:706146217213:web:d4fc66bf4915bedf752294",
     measurementId: "G-WYZ5WDXEC1"
   };
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const db = getFirestore(app);
-
-//TODO: Do your stuff here 
-async function getUser(db){
-    //...;
-}
+export const app = initializeApp(firebaseConfig);
