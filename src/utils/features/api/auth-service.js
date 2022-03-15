afe7a7d8874e63021c090f96a6a77e6b01622687
nikolayevolution <stu2001321014@uni-plovdiv.bg>
2022-03-15 14:16:50 +0200

    refactor: move firebase auth feature to auth-service.js

--- src/utils/features/api/auth-service.js
+++ src/utils/features/api/auth-service.js
@@ -1,9 +1,61 @@
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-
-async function signIn(email, password){
-
+/* eslint-disable no-unused-vars */
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword
+} from "firebase/auth";
+import { app } from "../firebase";
+const auth = getAuth(app);
+export async function signInWithEmail(email, password) {
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      const token = user.accessToken;
+      localStorage.setItem("user", JSON.stringify(user));
+      localStorage.setItem("Authenticated", true);
+      localStorage.setItem("token", token);
+      console.log(user);
+    })
+    .then(() => {
+        location.reload();
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
 
-async function signUp(fullName, email, password){
+export async function signInWithGoogle() {
+  const provider = new GoogleAuthProvider();
+  signInWithPopup(auth, provider).then((result) => {
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    const user = result.user;
+    localStorage.setItem("user", JSON.stringify(user));
+    localStorage.setItem("Authenticated", true);
+    localStorage.setItem("token", token);
+  })
+  .then(() => {
+      location.reload();
+  })
+  .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      const email = error.email;
+      const credential = GoogleAuthProvider.credentialFromError(error);
 
+      console.log(error);
+      console.log(errorMessage);
+  });
+}
+export async function signUpWithEmail(email, password) {
+    createUserWithEmailAndPassword(auth, email, password).then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user);
+    }).catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorMessage);
+    });
 }
