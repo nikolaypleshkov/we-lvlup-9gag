2cb194c33c9dbfdbccc12df4936c16240e25e6ed
nikolayevolution <stu2001321014@uni-plovdiv.bg>
2022-03-15 14:30:34 +0200

    Update index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-undef */
-import "./dom/app-navbar"
+import "./dom/app-navbar";
 import "./dom/app-drawer";
 import "./dom/app-signup";
 import "./dom/app-signin";
 import "./features/firebase";
-import "./features/auth_emulator_connect";
