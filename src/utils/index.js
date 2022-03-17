d7abcde24d941dded8a3dddcb3d484c54c9a855e
nikolayevolution <stu2001321014@uni-plovdiv.bg>
2022-03-17 17:08:03 +0200

    feat: import the new files

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,4 +3,6 @@ import "./dom/app-navbar";
 import "./dom/app-drawer";
 import "./dom/app-signup";
 import "./dom/app-signin";
+import "./dom/app-document";
+import "./dom/app-upload";
 import "./features/firebase";
