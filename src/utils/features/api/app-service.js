5dbd5ee9cb2d97232fdbf0284b0b0ee76f7f5a76
nikolayevolution <stu2001321014@uni-plovdiv.bg>
2022-03-17 17:03:53 +0200

    feat: Create a function to add post in firebase

--- src/utils/features/api/app-service.js
+++ src/utils/features/api/app-service.js
@@ -1,15 +1,26 @@
-async function postMeme(user){
-
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+import { db } from "../firebase";
+import { addDoc, collection } from "firebase/firestore";
+export async function postMeme(data) {
+  try {
+    const docRef = await addDoc(collection(db, "post"), {
+      img: data.img,
+      title: data.title,
+      description: data.description,
+      likes: data.likes,
+      comments: data.comment,
+      createdByUser: data.createdByUser,
+      createdOn: data.createdOn,
+    });
+    setTimeout(() => {
+      $("#uploadModal").modal("hide");
+    }, 500);
+  } catch (e) {
+    alert("Something went wrong: ", e);
+  }
 }
-async function viewMeme(id){
-
-}
-async function upvoteMeme(id){
-
-}
-async function downvoteMeme(id){
-
-}
-async function commentMeme(id, comment){
-
-}
+async function viewMeme(id) {}
+async function upvoteMeme(id) {}
+async function downvoteMeme(id) {}
+async function commentMeme(id, comment) {}
