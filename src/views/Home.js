b20ec9a2570aaedce8dc1342647feb8ba0f2a3c4
nikolayevolution <stu2001321014@uni-plovdiv.bg>
2022-03-25 19:51:03 +0200

    fix: fix the page components functions to replace the content of the  main section in index.html

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,15 +1,27 @@
 import AbstractView from "./AbstractView";
-
+import { collection, getDocs } from "firebase/firestore";
+import $ from "jquery";
+import { db } from "../utils/features/firebase";
 export default class extends AbstractView{
     constructor(){
         super();
         this.setTitle("7GAG - Home");
     }
-
     async getHtml(){
-        return /*html*/`
-            <div id="memeContainer"> 
-            </div>
-        `;
+        $("#content").empty();
+            try {
+                const querySnapshot = await getDocs(collection(db, "post"));
+                querySnapshot.forEach((doc) => {
+                    this.loadPosts(doc);
+                });
+              } catch (error) {
+                console.log("Error getting cached document: ", error);
+              }
     }
+    calculateTimeDiff(date) {
+        const today = new Date();
+        const diff = Math.abs(date - today) / (60*60*1000);
+        return Math.floor((diff/1000)/60);
+    }
+
 }
