ddc7f34908480709a2693d1a1624a9e702e4b5cd
nikolayevolution <stu2001321014@uni-plovdiv.bg>
2022-03-25 19:58:34 +0200

    feat: add display name and default photo to users who signed up with email and password

--- src/utils/features/api/auth-service.js
+++ src/utils/features/api/auth-service.js
@@ -4,22 +4,25 @@ import {
   signInWithEmailAndPassword,
   signInWithPopup,
   GoogleAuthProvider,
-  createUserWithEmailAndPassword
+  createUserWithEmailAndPassword,
+  updateProfile,
 } from "firebase/auth";
 import { app } from "../firebase";
+import $ from "jquery";
 const auth = getAuth(app);
 export async function signInWithEmail(email, password) {
   signInWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
       const user = userCredential.user;
+
       const token = user.accessToken;
       localStorage.setItem("user", JSON.stringify(user));
       localStorage.setItem("Authenticated", true);
       localStorage.setItem("token", user.uid);
       console.log(user);
     })
-    .then(() => {
-        location.reload();
+    .then(async () => {
+      location.reload();
     })
     .catch((error) => {
       console.log(error);
@@ -28,18 +31,19 @@ export async function signInWithEmail(email, password) {
 
 export async function signInWithGoogle() {
   const provider = new GoogleAuthProvider();
-  signInWithPopup(auth, provider).then((result) => {
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    const token = credential.accessToken;
-    const user = result.user;
-    localStorage.setItem("user", JSON.stringify(user));
-    localStorage.setItem("Authenticated", true);
-    localStorage.setItem("token", user.uid);
-  })
-  .then(() => {
+  signInWithPopup(auth, provider)
+    .then(async (result) => {
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      const user = result.user;
+      localStorage.setItem("user", JSON.stringify(user));
+      localStorage.setItem("Authenticated", true);
+      localStorage.setItem("token", user.uid);
+    })
+    .then(async () => {
       location.reload();
-  })
-  .catch((error) => {
+    })
+    .catch((error) => {
       const errorCode = error.code;
       const errorMessage = error.message;
       const email = error.email;
@@ -47,15 +51,25 @@ export async function signInWithGoogle() {
 
       console.log(error);
       console.log(errorMessage);
-  });
+    });
 }
-export async function signUpWithEmail(email, password) {
-    createUserWithEmailAndPassword(auth, email, password).then((userCredential) => {
-        const user = userCredential.user;
-        console.log(user);
-    }).catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log(errorMessage);
+export async function signUpWithEmail(email, password, displayName) {
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      updateProfile(user, {
+        displayName: displayName,
+        photoURL:
+          "https://www.kindpng.com/picc/m/24-248253_user-profile-default-image-png-clipart-png-download.png",
+      });
+    }).then(() => location.reload())
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorMessage);
+      $(/*html*/ `
+        <div class="alert alert-danger mt-2">
+          <span>${errorMessage}</span>
+        </div>`).insertBefore("#signupForm");
     });
 }
