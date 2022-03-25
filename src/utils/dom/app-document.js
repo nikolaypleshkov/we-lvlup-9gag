6b65cb7a850ccf5586d35b85bdbdd794ccabde4e
nikolayevolution <stu2001321014@uni-plovdiv.bg>
2022-03-25 19:54:50 +0200

    fix: fix like feature and add some additional styling

--- src/utils/dom/app-document.js
+++ src/utils/dom/app-document.js
@@ -6,86 +6,95 @@ $(document).ready(async function () {
   $(window).scroll(async function () {
     if ($(this).scrollTop() == $(document).height() - $(window).height()) {
       //TODO: Make call to giphy to fetch memes
-      try {
-        const querySnapshot = await getDocs(collection(db, "post"));
-        querySnapshot.forEach((doc) => {
-          appendToHtml(doc);
-        });
-      } catch (error) {
-        console.log("Error getting cached document: ", error);
+      if(location.hash !== "#post"){
+        try {
+          const querySnapshot = await getDocs(collection(db, "post"));
+          querySnapshot.forEach((doc) => {
+            appendToHtml(doc);
+          });
+        } catch (error) {
+          console.log("Error getting cached document: ", error);
+        }
       }
     }
+  });
+  $(document).on("click", ".nav-btn", function(){
+    $(".nav-btn").removeClass("selected");
+    $(this).addClass("selected");
   });
   $(document).on("click", "#upVote", async function () {
     const isAuth = localStorage.getItem("Authenticated");
     if (isAuth) {
+      const button = $(this);
       const elementId = $(this).parent().parent().attr("data-eid");
-      const btnColor = $(this).children("svg");
+      const btnSVG = $(this).children("svg");
       let btnValue = $(this).children(".upvotes-info").children("#likesNumber");
       const likeRef = doc(db, "post", elementId);
       const docRef = await getDoc(likeRef);
       let likesID = docRef.get("likesID");
       let isLiked = false;
       const currentUserId = localStorage.getItem("token");
-      likesID.forEach((id) => {
-         if(id == currentUserId) isLiked = true;
-      });
+      if(likesID.length > 0){
+        likesID.forEach((id) => {
+           if(id == currentUserId) isLiked = true;
+        });
+      }
 
       if(isLiked){
         likesID = likesID.filter((id) => {
           return id != currentUserId;
         });
         await setDoc(likeRef, {likes: likesID.length,  likesID: likesID }, { merge: true });
-        $(btnColor).attr("fill", "#999");
+        $(button).css("background", "#fff");
+        $(btnSVG).attr("fill", "#999");
         $(btnValue).text(likesID.length);
       }
       else{
         likesID.push(currentUserId);
         await setDoc(likeRef, { likes: likesID.length, likesID: likesID }, { merge: true });
-        $(btnColor).attr("fill", "red");
+        $(button).css("background", "rgba(222, 222, 254, 0.72)");
+        $(btnSVG).attr("fill", "blue");
         $(btnValue).text(likesID.length);
       }
-
-      // if ($(btnColor).attr("fill") === "red") {
-      //   let value = parseInt(
-      //     $(this).children(".upvotes-info").children("#likesNumber").text()
-      //   );
-      //   value = value - 1;
-      //   $(btnValue).text(value);
-      //   $(btnColor).attr("fill", "#999");
-      //   try {
-      //     console.log(elementId);
-      //     // const likeRef = db.collection("post").doc(elementId);
-      //     const likeRef = doc(db, "post", elementId);
-      //     const res = await setDoc(likeRef, { likes: value }, { merge: true });
-      //     console.log("Reuslt: ", res);
-      //   } catch (error) {
-      //     console.log("Something went wrong when updating: ", error);
-      //   }
-      // } else {
-      //   let value = parseInt(
-      //     $(this).children(".upvotes-info").children("#likesNumber").text()
-      //   );
-      //   value = value + 1;
-      //   $(btnColor).attr("fill", "red");
-      //   $(btnValue).text(value);
-      //   try {
-      //     console.log(elementId);
-      //     likesID.push(localStorage.getItem("token"));
-      //     console.log(likesID);
-      //     await setDoc(likeRef, { likes: value, likesID: likesID }, { merge: true });
-      //   } catch (error) {
-      //     console.log("Something went wrong when updating: ", error);
-      //   }
-      // }
     }
     else {
       alert("You need to be logged in first.");
     }
   });
 
+  $(document).on("click", "#downVote", async function(){
+    const isAuth = localStorage.getItem("Authenticated");
+    if(isAuth){    
+      const elementId = $(this).parent().parent().attr("data-eid");
+      const upVoteButton = $(this).parent().children("#upVote");
+      console.log(elementId);
+      const likeRef = doc(db, "post", elementId);
+      const docRef = await getDoc(likeRef);
+      let likesID = docRef.get("likesID");
+      let isLiked = false;
+      const currentUserId = localStorage.getItem("token");
+      if(likesID.length > 0){
+        likesID.forEach((id) => {
+           if(id == currentUserId) isLiked = true;
+        });
+      }
+      if(isLiked){
+        likesID = likesID.filter((id) => {
+          return id != currentUserId;
+        });
+        await setDoc(likeRef, {likes: likesID.length,  likesID: likesID }, { merge: true });
+        $(upVoteButton).css("background", "#fff");
+        $(upVoteButton).children("svg").attr("fill", "#999");
+        $(upVoteButton).children(".upvotes-info").children("#likesNumber").text(likesID.length);
+      }
+    }
+    else {
+      alert("you need to logged in firs.");
+    }
+  });
+
   function appendToHtml(data) {
-    $("#memeContainer").append(/*html*/ `
+    $("#content").append(/*html*/ `
   <div class="memeCard mt-5" data-eid="${data.id}"> 
           <div class="user-info d-flex"> 
             <img src="${data.data().createdByUser.photoURL}" height="25px" width="25px" />
@@ -94,15 +103,17 @@ $(document).ready(async function () {
               <span>· ${calculateTimeDiff(data.data().createdOn)} min</span>
             </p>
           </div> 
-        <a class="post-header" href="#postid">${data.data().title}</a>
+        <a class="post-header" data-link href="/#post" id="getId">${data.data().title}</a>
       <div class="memeContent"> 
-          <img class="giftplayer" data-src src="${
-            data.data().img
-          }" alt="meme" />
+      <a class="img-link" data-link href="/#post" id="getIdImg"> 
+      <img class="giftplayer" data-src src="${
+        data.data().img
+      }" alt="meme" />
+      </a> 
       </div>
       <div class="button-container d-flex mt-2"> 
-          <button class="btn post-btn d-flex align-items-center" id="upVote">
-            <svg xmlns="http://www.w3.org/2000/svg" width="18px" height="18px" fill="${getColor(data.data().likesID)}" viewBox="0 0 24 24"><path d="M4 14h4v7a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-7h4a1.001 1.001 0 0 0 .781-1.625l-8-10c-.381-.475-1.181-.475-1.562 0l-8 10A1.001 1.001 0 0 0 4 14z"/></svg>
+      <button class="btn post-btn d-flex align-items-center" id="upVote" style="${data.data().likesID.includes(localStorage.getItem("token")) ? "background: rgba(222, 222, 254, 0.72);" : "background: #fff}"}">
+      <svg xmlns="http://www.w3.org/2000/svg" width="18px" height="18px" fill="${data.data().likesID.includes(localStorage.getItem("token")) ? "blue" : "#999"}" viewBox="0 0 24 24"><path d="M4 14h4v7a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-7h4a1.001 1.001 0 0 0 .781-1.625l-8-10c-.381-.475-1.181-.475-1.562 0l-8 10A1.001 1.001 0 0 0 4 14z"/></svg>
             <div class="upvotes-info"> 
               <span class="text-muted" id="likesNumber">${
                 data.data().likes
@@ -129,13 +140,5 @@ $(document).ready(async function () {
     const today = new Date();
     var diff = Math.abs(date - today) / (60*60*1000);
     return Math.floor((diff/1000)/60);
-  }
-
-  function getColor(data) {
-   const userId = localStorage.getItem("token");
-   const isLiked = data.includes(userId);
-   if(isLiked) return "red";
-   return "#999";
-    
   }
 });
