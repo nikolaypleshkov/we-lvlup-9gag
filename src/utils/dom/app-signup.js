78eccaecbafe830b14b47c775384165defd83f57
nikolayevolution <stu2001321014@uni-plovdiv.bg>
2022-03-25 19:56:05 +0200

    feat: add credentials validation for the signup

--- src/utils/dom/app-signup.js
+++ src/utils/dom/app-signup.js
@@ -7,11 +7,17 @@ $(document).ready(function () {
   const fullName = $("#fullName");
   const email = $("#username");
   const password = $("#password");
-  var dangerAlert = $("#danger-email");
-  $(dangerAlert).hide();
-
-  $(fullName).on("blur", () => {
-    console.log($(fullName).val());
+  const dangerEmail = $("#danger-email");
+  const dangerPassword = $("#danger-password");
+  const dangerUsername = $("#danger-username");
+  const dangerCredit = $("#danger-credit");
+  let isUsernameVaild, isEmailValid, isPasswordValid = false;
+  $(dangerEmail).hide();
+  $(dangerPassword).hide();
+  $(dangerUsername).hide();
+  $(dangerCredit).hide();
+  $(fullName).on("change", () => {
+    validateUsername();
   });
 
   $(email).on("change", () => {
@@ -22,26 +28,49 @@ $(document).ready(function () {
     validatePassword();
   });
 
+  function validateUsername() {
+    let userInput = $(fullName).val();
+    if(userInput.length <= 2){
+      $(dangerUsername).show();
+      isUsernameVaild = true;
+    }
+    else {
+      $(dangerUsername).hide();
+      isUsernameVaild = true;
+    }
+  }
+
   function validatePassword() {
     let userInput = $(password).val();
-    //TODO: Check if password is valid
-    console.log(userInput);
+    if(userInput.length <= 5){
+      $(dangerPassword).show();
+      isPasswordValid = false;
+    }
+    else {
+      $(dangerPassword).hide();
+      isPasswordValid = true;
+    }
   }
   function validateEmail() {
     let userInput = $(email).val();
     let regex =
       /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (!userInput.match(regex)) {
-      $(dangerAlert).show();
+      $(dangerEmail).show();
+      isEmailValid = false;
     } else {
-      $(dangerAlert).hide();
+      $(dangerEmail).hide();
+      isEmailValid = true;
     }
-    //TODO:
-    //Check if Email is valid => if its not valid show danger alert
-    console.log(userInput);
   }
   $(form).on("submit", (e) => {
     e.preventDefault();
-    signUpWithEmail($(email).val(), $(password).val());
+    if(isUsernameVaild && isEmailValid && isPasswordValid){
+      $(dangerCredit).hide();
+      signUpWithEmail($(email).val(), $(password).val(), $(fullName).val());
+    }
+    else {
+      $(dangerCredit).show();
+    }
   });
 });
