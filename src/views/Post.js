3cd0cb58ef1a6de2421589ddeb4b23c612cac3c2
nikolayevolution <stu2001321014@uni-plovdiv.bg>
2022-03-28 22:37:45 +0300

    feat: append comments for current post

--- src/views/Post.js
+++ src/views/Post.js
@@ -66,15 +66,53 @@ export default class extends AbstractView {
       <section class="comments-section-container"> 
             <section class="comment-section-input"> 
                 <div class="comment-input-area"> 
-                    <textarea class="comment-textarea" placeholder="Write a comment..."></textarea>
+                    <textarea class="comment-textarea" placeholder="Write a comment..." id="userComment"></textarea>
                 </div>
             </section>
             <footer class="comments-section-footer"> 
                 <section class="comments-action-area"> 
                     <button class="btn text-muted">Cancel</button>
-                    <button class="btn btn-primary">Post</button>
+                    <button class="btn btn-primary" id="postButton">Post</button>
                 </section>
             </footer>
+      </section>
+      <section class="comments-list-container" id="commentsContainer"> 
+      ${data.data().comments.map((comment) => {
+        return /*html*/ `
+        <div class="comment-card-wrapper"> 
+        <div class="user-profile-avatar d-flex mt-2"> 
+          <img src="${
+           comment.user.photoURL
+            }" height="25px" width="25px" />
+            <p class="owner-info text-muted"> 
+              ${comment.user.displayName}
+            </p>
+        </div>
+        <div class="comment-list-item"> 
+          <div class="comment-list-item__text">${comment.comment}</div>
+          <footer class="comment-list-item__footer"> 
+            
+        <div class="button-container d-flex mt-2"> 
+        <button class="btn post-btn d-flex align-items-center" id="upVote" >
+        <svg xmlns="http://www.w3.org/2000/svg" width="18px" height="18px" fill="#999" viewBox="0 0 24 24"><path d="M4 14h4v7a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-7h4a1.001 1.001 0 0 0 .781-1.625l-8-10c-.381-.475-1.181-.475-1.562 0l-8 10A1.001 1.001 0 0 0 4 14z"/></svg>
+              <div class="upvotes-info"> 
+                <span class="text-muted" id="likesNumber">0</span>
+              </div>
+              </button>
+            <button class="btn post-btn d-flex align-items-center" id="downVote">
+              <svg xmlns="http://www.w3.org/2000/svg" width="18px" height="18px" fill="#999" viewBox="0 0 24 24"><path d="M20.901 10.566A1.001 1.001 0 0 0 20 10h-4V3a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v7H4a1.001 1.001 0 0 0-.781 1.625l8 10a1 1 0 0 0 1.562 0l8-10c.24-.301.286-.712.12-1.059z"/></svg>
+              <div class="downvotes-info"> 
+                <span class="text-muted">0</span>
+              </div>
+              </button>
+        </div>
+          </footer>
+        </div>
+      </div>
+        
+        `;
+      })}
+        
       </section>
     `);
   }
