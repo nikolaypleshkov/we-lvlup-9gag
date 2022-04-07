46260c14a9c0eaaeb6d322a615dad5f8d18417ff
nikolayevolution <stu2001321014@uni-plovdiv.bg>
2022-04-07 18:28:18 +0300

    fix: fixed upvote and downvote buttons in Post.js page

--- src/views/Post.js
+++ src/views/Post.js
@@ -14,6 +14,7 @@ export default class extends AbstractView {
       const docRef = doc(db, "post", this.id);
       const docSnapshot = await getDoc(docRef);
       this.loadPost(docSnapshot);
+      this.returnComments(docSnapshot);
     } catch (error) {
       console.log("Error getting post for current id", error);
     }
@@ -25,7 +26,7 @@ export default class extends AbstractView {
           <div class="user-info d-flex"> 
             <img src="${
               data.data().createdByUser.photoURL
-            }" height="25px" width="25px" />
+            }" height="25px" width="25px" style="border-radius: 50%;" />
             <p class="owner-info text-muted"> 
               ${data.data().createdByUser.displayName}
             </p>
@@ -33,25 +34,35 @@ export default class extends AbstractView {
             <h2>${data.data().title}</h2>
           <div class="memeContent">
           <div class="button-container d-flex mt-2"> 
-          <button class="btn post-btn d-flex align-items-center" id="upVote" style="${data.data().likesID.includes(localStorage.getItem("token")) ? "background: rgba(222, 222, 254, 0.72);" : "background: #fff}"}">
-          <svg xmlns="http://www.w3.org/2000/svg" width="18px" height="18px" fill="${data.data().likesID.includes(localStorage.getItem("token")) ? "blue" : "#999"}" viewBox="0 0 24 24"><path d="M4 14h4v7a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-7h4a1.001 1.001 0 0 0 .781-1.625l-8-10c-.381-.475-1.181-.475-1.562 0l-8 10A1.001 1.001 0 0 0 4 14z"/></svg>
+          <button class="btn post-btn d-flex align-items-center" id="upVote" style="${
+            data.data().likesID.includes(localStorage.getItem("token"))
+              ? "background: rgba(222, 222, 254, 0.72);"
+              : "background: #fff}"
+          }">
+          <svg xmlns="http://www.w3.org/2000/svg" width="18px" height="18px" fill="${
+            data.data().likesID.includes(localStorage.getItem("token"))
+              ? "blue"
+              : "#999"
+          }" viewBox="0 0 24 24"><path d="M4 14h4v7a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-7h4a1.001 1.001 0 0 0 .781-1.625l-8-10c-.381-.475-1.181-.475-1.562 0l-8 10A1.001 1.001 0 0 0 4 14z"/></svg>
                 <div class="upvotes-info"> 
                   <span class="text-muted" id="likesNumber">${
                     data.data().likes
                   }</span>
                 </div>
                 </button>
-              <button class="btn post-btn d-flex align-items-center" id="downVote">
-                <svg xmlns="http://www.w3.org/2000/svg" width="18px" height="18px" fill="#999" viewBox="0 0 24 24"><path d="M20.901 10.566A1.001 1.001 0 0 0 20 10h-4V3a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v7H4a1.001 1.001 0 0 0-.781 1.625l8 10a1 1 0 0 0 1.562 0l8-10c.24-.301.286-.712.12-1.059z"/></svg>
+              <button class="btn post-btn d-flex align-items-center" id="downVote" style="${
+                data.data().dislikesID.includes(localStorage.getItem("token"))
+                  ? "background: rgb(255, 153, 153);"
+                  : "background: #fff}"
+              }">
+                <svg xmlns="http://www.w3.org/2000/svg" width="18px" height="18px" fill="${
+                  data.data().dislikesID.includes(localStorage.getItem("token"))
+                    ? "red"
+                    : "#999"
+                }"  viewBox="0 0 24 24"><path d="M20.901 10.566A1.001 1.001 0 0 0 20 10h-4V3a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v7H4a1.001 1.001 0 0 0-.781 1.625l8 10a1 1 0 0 0 1.562 0l8-10c.24-.301.286-.712.12-1.059z"/></svg>
                 <div class="downvotes-info"> 
                   <span class="text-muted">${data.data().dislikes}</span>
                 </div>
-                </button>
-                <button class="btn post-btn d-flex align-items-center" id="commentsBtn"> 
-                    <i class="bi bi-chat-right-fill"></i>
-                    <span class="text-muted">${
-                      data.data().comments.length
-                    }</span>
                 </button>
           </div>
           <div class="img-link"> 
@@ -77,43 +88,65 @@ export default class extends AbstractView {
             </footer>
       </section>
       <section class="comments-list-container" id="commentsContainer"> 
-      ${data.data().comments.map((comment) => {
-        return (/*html*/ `
-        <div class="comment-card-wrapper"> 
+      </section>
+    `);
+  }
+
+  returnComments(data) {
+    const comments = data.data().comments.map((comment) => {
+      return comment;
+    });
+    comments.forEach((comment) => {
+      $("#commentsContainer").append(/*html*/ `
+    <div class="comment-card-wrapper"> 
         <div class="user-profile-avatar d-flex mt-2"> 
-          <img no-referrer src="${
-           comment.user.photoURL
-            }" height="25px" width="25px" />
+          <img no-referrer src="${comment.user.photoURL}" height="25px" width="25px" style="border-radius: 50%;" />
             <p class="owner-info text-muted"> 
               ${comment.user.displayName}
             </p>
         </div>
-        <div class="comment-list-item"> 
+        <div class="comment-list-item" data-eid="${comment.uuid}"> 
           <div class="comment-list-item__text">
           <span id="userComment__text">${comment.comment}</span>
           </div>
           <footer class="comment-list-item__footer"> 
             
         <div class="button-container d-flex mt-2"> 
-        <button class="btn post-btn d-flex align-items-center" id="upVote" >
-        <svg xmlns="http://www.w3.org/2000/svg" width="18px" height="18px" fill="#999" viewBox="0 0 24 24"><path d="M4 14h4v7a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-7h4a1.001 1.001 0 0 0 .781-1.625l-8-10c-.381-.475-1.181-.475-1.562 0l-8 10A1.001 1.001 0 0 0 4 14z"/></svg>
+        <button class="btn post-btn d-flex align-items-center" id="upVoteComment" style="${comment.likesID.includes(localStorage.getItem("token")) ? "background: rgba(222, 222, 254, 0.72);" : "background: #fff}"}"  >
+        <svg xmlns="http://www.w3.org/2000/svg" width="18px" height="18px" fill="${
+          comment.likesID.includes(localStorage.getItem("token"))
+            ? "blue"
+            : "#999"
+        }" viewBox="0 0 24 24"><path d="M4 14h4v7a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-7h4a1.001 1.001 0 0 0 .781-1.625l-8-10c-.381-.475-1.181-.475-1.562 0l-8 10A1.001 1.001 0 0 0 4 14z"/></svg>
               <div class="upvotes-info"> 
-                <span class="text-muted" id="likesNumber">0</span>
+                <span class="text-muted" id="likesNumber">${
+                  comment.likes
+                }</span>
               </div>
               </button>
-            <button class="btn post-btn d-flex align-items-center" id="downVote">
-              <svg xmlns="http://www.w3.org/2000/svg" width="18px" height="18px" fill="#999" viewBox="0 0 24 24"><path d="M20.901 10.566A1.001 1.001 0 0 0 20 10h-4V3a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v7H4a1.001 1.001 0 0 0-.781 1.625l8 10a1 1 0 0 0 1.562 0l8-10c.24-.301.286-.712.12-1.059z"/></svg>
+            <button class="btn post-btn d-flex align-items-center" id="downVoteComment" style="${
+              comment.dislikesID.includes(localStorage.getItem("token"))
+                ? "background: rgb(255, 153, 153);"
+                : "background: #fff}"
+            }">
+              <svg xmlns="http://www.w3.org/2000/svg" width="18px" height="18px" fill="${
+                comment.dislikesID.includes(localStorage.getItem("token"))
+                  ? "red"
+                  : "#999"
+              }" viewBox="0 0 24 24"><path d="M20.901 10.566A1.001 1.001 0 0 0 20 10h-4V3a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v7H4a1.001 1.001 0 0 0-.781 1.625l8 10a1 1 0 0 0 1.562 0l8-10c.24-.301.286-.712.12-1.059z"/></svg>
               <div class="downvotes-info"> 
-                <span class="text-muted">0</span>
+                <span class="text-muted">
+                ${
+                  comment.dislikes
+                }
+                </span>
               </div>
               </button>
         </div>
           </footer>
         </div>
       </div>
-        `);
-      })}
-      </section>
     `);
+    });
   }
 }
