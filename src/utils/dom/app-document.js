ab8dafebbc95f42c1877053bf94cc8a9fb00a3cf
nikolayevolution <stu2001321014@uni-plovdiv.bg>
2022-04-07 18:29:39 +0300

    fix: fixed upvote and downvote feature and fixed the dark theme function

--- src/utils/dom/app-document.js
+++ src/utils/dom/app-document.js
@@ -3,22 +3,27 @@ import $ from "jquery";
 import { collection, doc, getDoc, getDocs, setDoc } from "firebase/firestore";
 import { db } from "../features/firebase";
 $(document).ready(async function () {
+  $("html").attr("data-theme", localStorage.getItem("theme"));
+  if (localStorage.getItem("theme") == "dark") {
+    $("#darkModeCheckbox").attr("checked", "checked");
+  }
+
   $(window).scroll(async function () {
     if ($(this).scrollTop() == $(document).height() - $(window).height()) {
       //TODO: Make call to giphy to fetch memes
-      if(location.hash !== "#post" && location.hash !== "#account"){
+      if (location.hash !== "#post" && location.hash !== "#account") {
         try {
           const querySnapshot = await getDocs(collection(db, "post"));
           querySnapshot.forEach((doc) => {
             appendToHtml(doc);
           });
         } catch (error) {
-          console.log("Error getting cached document: ", error);
+          alert("Error getting cached document: ", error);
         }
       }
     }
   });
-  $(document).on("click", ".nav-btn", function(){
+  $(document).on("click", ".nav-btn", function () {
     $(".nav-btn").removeClass("selected");
     $(this).addClass("selected");
   });
@@ -27,7 +32,9 @@ $(document).ready(async function () {
     if (isAuth) {
       const button = $(this);
       const downVoteButton = $(this).parent().children("#downVote");
-      const elementId = $(this).parent().parent().attr("data-eid");
+      const elementId = localStorage.getItem("postId")
+        ? localStorage.getItem("postId")
+        : $(this).parent().parent().attr("data-eid");
       const btnSVG = $(this).children("svg");
       let btnValue = $(this).children(".upvotes-info").children("#likesNumber");
       const likeRef = doc(db, "post", elementId);
@@ -37,56 +44,70 @@ $(document).ready(async function () {
       let isLiked = false;
       let isDisliked = false;
       const currentUserId = localStorage.getItem("token");
-      if(likesID.length > 0){
+      if (likesID.length > 0) {
         likesID.forEach((id) => {
-           if(id == currentUserId) isLiked = true;
+          if (id == currentUserId) isLiked = true;
         });
       }
-      if(dislikesID.length > 0){
+      if (dislikesID.length > 0) {
         dislikesID.forEach((id) => {
-          if(id == currentUserId) isDisliked = true;
+          if (id == currentUserId) isDisliked = true;
         });
       }
-      if(isLiked){
+      if (isLiked) {
         likesID = likesID.filter((id) => {
           return id != currentUserId;
         });
-        await setDoc(likeRef, {likes: likesID.length,  likesID: likesID }, { merge: true });
+        await setDoc(
+          likeRef,
+          { likes: likesID.length, likesID: likesID },
+          { merge: true }
+        );
         $(button).css("background", "#fff");
         $(btnSVG).attr("fill", "#999");
         $(btnValue).text(likesID.length);
-      }
-      else{
-        if(isDisliked){
+      } else {
+        if (isDisliked) {
           dislikesID = dislikesID.filter((id) => {
-          return id != currentUserId;
-        });
-        await setDoc(likeRef, {dislikes: dislikesID.length,  dislikesID: dislikesID }, { merge: true });
-        $(downVoteButton).css("background", "#fff");
-        $(downVoteButton).children("svg").attr("fill", "#999");
-        $(downVoteButton).children(".downvotes-info").children("span").text(dislikesID.length);
+            return id != currentUserId;
+          });
+          await setDoc(
+            likeRef,
+            { dislikes: dislikesID.length, dislikesID: dislikesID },
+            { merge: true }
+          );
+          $(downVoteButton).css("background", "#fff");
+          $(downVoteButton).children("svg").attr("fill", "#999");
+          $(downVoteButton)
+            .children(".downvotes-info")
+            .children("span")
+            .text(dislikesID.length);
         }
         likesID.push(currentUserId);
-        await setDoc(likeRef, { likes: likesID.length, likesID: likesID }, { merge: true });
+        await setDoc(
+          likeRef,
+          { likes: likesID.length, likesID: likesID },
+          { merge: true }
+        );
         $(button).css("background", "rgba(222, 222, 254, 0.72)");
         $(btnSVG).attr("fill", "blue");
         $(btnValue).text(likesID.length);
       }
-    }
-    else {
+    } else {
       alert("You need to be logged in first.");
     }
   });
 
-  $(document).on("click", "#downVote", async function(){
+  $(document).on("click", "#downVote", async function () {
     const isAuth = localStorage.getItem("Authenticated");
-    if(isAuth){    
-      const elementId = $(this).parent().parent().attr("data-eid");
+    if (isAuth) {
+      const elementId = localStorage.getItem("postId")
+        ? localStorage.getItem("postId")
+        : $(this).parent().parent().attr("data-eid");
       const upVoteButton = $(this).parent().children("#upVote");
       const downVoteButton = $(this);
       const btnSvg = $(this).children("svg");
       let btnValue = $(this).children(".downvotes-info").children("span");
-      console.log(elementId);
       const likeRef = doc(db, "post", elementId);
       const docRef = await getDoc(likeRef);
       let likesID = docRef.get("likesID");
@@ -94,50 +115,66 @@ $(document).ready(async function () {
       let isLiked = false;
       let isDisliked = false;
       const currentUserId = localStorage.getItem("token");
-      if(likesID.length > 0){
+      if (likesID.length > 0) {
         likesID.forEach((id) => {
-           if(id == currentUserId) isLiked = true;
+          if (id == currentUserId) isLiked = true;
         });
       }
-      if(dislikesID.length > 0){
+      if (dislikesID.length > 0) {
         dislikesID.forEach((id) => {
-          if(id == currentUserId) isDisliked = true;
+          if (id == currentUserId) isDisliked = true;
         });
       }
-      if(isLiked){
+      if (isLiked) {
         likesID = likesID.filter((id) => {
           return id != currentUserId;
         });
-        dislikesID.push(currentUserId );
-        await setDoc(likeRef, {likes: likesID.length,  likesID: likesID }, { merge: true });
+        dislikesID.push(currentUserId);
+        await setDoc(
+          likeRef,
+          { likes: likesID.length, likesID: likesID },
+          { merge: true }
+        );
         $(upVoteButton).css("background", "#fff");
         $(upVoteButton).children("svg").attr("fill", "#999");
-        $(upVoteButton).children(".upvotes-info").children("#likesNumber").text(likesID.length);
-        await setDoc(likeRef, {dislikes: dislikesID.length,  dislikesID: dislikesID }, { merge: true });
+        $(upVoteButton)
+          .children(".upvotes-info")
+          .children("#likesNumber")
+          .text(likesID.length);
+        await setDoc(
+          likeRef,
+          { dislikes: dislikesID.length, dislikesID: dislikesID },
+          { merge: true }
+        );
         $(downVoteButton).css("background", "rgb(255, 153, 153)");
         $(btnSvg).attr("fill", "red");
         $(btnValue).text(dislikesID.length);
-      }
-      else {
-        if(isDisliked){
+      } else {
+        if (isDisliked) {
           dislikesID = dislikesID.filter((id) => {
             return id != currentUserId;
           });
-          await setDoc(likeRef, {dislikes: dislikesID.length,  dislikesID: dislikesID }, { merge: true });
+          await setDoc(
+            likeRef,
+            { dislikes: dislikesID.length, dislikesID: dislikesID },
+            { merge: true }
+          );
           $(downVoteButton).css("background", "#fff");
           $(btnSvg).attr("fill", "#999");
           $(btnValue).text(dislikesID.length);
-        }
-        else {
+        } else {
           dislikesID.push(currentUserId);
-          await setDoc(likeRef, {dislikes: dislikesID.length,  dislikesID: dislikesID }, { merge: true });
+          await setDoc(
+            likeRef,
+            { dislikes: dislikesID.length, dislikesID: dislikesID },
+            { merge: true }
+          );
           $(downVoteButton).css("background", "rgb(255, 153, 153)");
           $(btnSvg).attr("fill", "red");
           $(btnValue).text(dislikesID.length);
         }
       }
-    }
-    else {
+    } else {
       alert("you need to logged in firs.");
     }
   });
@@ -146,23 +183,33 @@ $(document).ready(async function () {
     $("#content").append(/*html*/ `
   <div class="memeCard mt-5" data-eid="${data.id}"> 
           <div class="user-info d-flex"> 
-            <img no-referrer src="${data.data().createdByUser.photoURL}" height="25px" width="25px" />
+            <img no-referrer src="${
+              data.data().createdByUser.photoURL
+            }" height="25px" width="25px" style="border-radius: 50%;" />
             <p class="owner-info text-muted"> 
               <a href="#user">${data.data().createdByUser.displayName}</a>
               <span>· ${calculateTimeDiff(data.data().createdOn)} min</span>
             </p>
           </div> 
-        <a class="post-header" data-link href="/#post" id="getId">${data.data().title}</a>
+        <a class="post-header" data-link href="/#post" id="getId">${
+          data.data().title
+        }</a>
       <div class="memeContent"> 
       <a class="img-link" data-link href="/#post" id="getIdImg"> 
-      <img class="giftplayer" data-src src="${
-        data.data().img
-      }" alt="meme" />
+      <img class="giftplayer" data-src src="${data.data().img}" alt="meme" />
       </a> 
       </div>
       <div class="button-container d-flex mt-2"> 
-      <button class="btn post-btn d-flex align-items-center" id="upVote" style="${data.data().likesID.includes(localStorage.getItem("token")) ? "background: rgba(222, 222, 254, 0.72);" : "background: #fff}"}">
-      <svg xmlns="http://www.w3.org/2000/svg" width="18px" height="18px" fill="${data.data().likesID.includes(localStorage.getItem("token")) ? "blue" : "#999"}" viewBox="0 0 24 24"><path d="M4 14h4v7a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-7h4a1.001 1.001 0 0 0 .781-1.625l-8-10c-.381-.475-1.181-.475-1.562 0l-8 10A1.001 1.001 0 0 0 4 14z"/></svg>
+      <button class="btn post-btn d-flex align-items-center" id="upVote" style="${
+        data.data().likesID.includes(localStorage.getItem("token"))
+          ? "background: rgba(222, 222, 254, 0.72);"
+          : "background: #fff}"
+      }">
+      <svg xmlns="http://www.w3.org/2000/svg" width="18px" height="18px" fill="${
+        data.data().likesID.includes(localStorage.getItem("token"))
+          ? "blue"
+          : "#999"
+      }" viewBox="0 0 24 24"><path d="M4 14h4v7a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-7h4a1.001 1.001 0 0 0 .781-1.625l-8-10c-.381-.475-1.181-.475-1.562 0l-8 10A1.001 1.001 0 0 0 4 14z"/></svg>
             <div class="upvotes-info"> 
               <span class="text-muted" id="likesNumber">${
                 data.data().likes
@@ -175,10 +222,10 @@ $(document).ready(async function () {
               <span class="text-muted">${data.data().dislikes}</span>
             </div>
             </button>
-            <button class="btn post-btn d-flex align-items-center" id="commentsBtn"> 
+            <a class="btn post-btn d-flex align-items-center" role="button" data-link href="/#post" id="getId"> 
                 <i class="bi bi-chat-right-fill"></i>
                 <span class="text-muted">${data.data().comments.length}</span>
-            </button>
+            </a>
       </div>
   </div>
   <hr class="mt-2 mb-3"/>
@@ -187,7 +234,7 @@ $(document).ready(async function () {
 
   function calculateTimeDiff(date) {
     const today = new Date();
-    var diff = Math.abs(date - today) / (60*60*1000);
-    return Math.floor((diff/1000)/60);
+    var diff = Math.abs(date - today) / (60 * 60 * 1000);
+    return Math.floor(diff / 1000 / 60);
   }
 });
